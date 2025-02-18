47ea813428e21a3adfda0bb26095594b43618921
ashish <a.asis.sigdel01@gmail.com>
2025-02-18 14:40:10 +0545

    again update for upload

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 
 import authRoute from "./auth.routes";
+import uploadRoute from "./upload.routes";
 
 const router = express.Router();
 
 router.use("/auth", authRoute);
+router.use("/upload", uploadRoute);
 
 export default router;
